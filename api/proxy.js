deeddda71ec7c32fa8186ec6ab40200b61aba839
Corn Pig <baggio326407@outlook.com>
2025-11-16 22:48:51 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,16 +1,8 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://www.google.com/";//your website url
-  //   if (
-  //     req.url.startsWith("/api") ||
-  //     req.url.startsWith("/auth") ||
-  //     req.url.startsWith("/banner") ||
-  //     req.url.startsWith("/CollegeTask")
-  //   ) {
-  //     target = "http://106.15.2.32:6969";
-  //   }
-
+  let target = "https:/wiki.backroomszh.org/";//your website url
+  
   createProxyMiddleware({
     target,
     changeOrigin: true,
